288dc5f9bf8f25f5a253e90a423f92e52fbc1d84
Joey Picca <joeypicca@gmail.com>
2019-12-16 10:38:48 -0600

    Removed bug in average change calc and added greatest increase/decrease

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,23 +9,38 @@ with open(fname, 'r') as csvfile:
     header = next(csvreader)
     revenue = 0
     totalChange = 0
-    largestProfit = 0
-    largestLoss = 0
+    largestIncrease = 0
+    largestDecrease = 0
     
     for i, row in enumerate(csvreader):
         #print(f'{i}: {row}')
         revenue += int(row[1])
         
         if i > 0:
-            totalChange += int(row[1]) - int(old_row)
-        
+            change = int(row[1]) - int(old_row)
+            totalChange += change
+            
+            if change > largestIncrease:
+                largestIncrease = change
+
+            if change < largestDecrease:
+                largestDecrease = change
+            
+        #print(int(row[1]) > largestProfit)
+        #Before next iteration, save current iteration's revenue to "old row" so we can calculate change
         old_row = row[1]
         
     monthCount = i + 1
-    avgChange = totalChange / monthCount
+
+    # Subtract 1 from monthCount to get the number of changes
+    avgChange = totalChange / (monthCount - 1)
 
 
-# Debug
+# Printing to Terminal
+print('Financial Analysis')
+print('------------------')
 print(f'Total Months: {monthCount}')
-print(f'Total Revenue: {revenue}')
-print(f'Average Change: {avgChange}')
+print(f'Total: ${revenue}')
+print(f'Average Change: ${avgChange:.2f}')
+print(f'Greatest Increase in Profits: ${largestIncrease}')
+print(f'Greatest Decrease in Profits: ${largestDecrease}')
